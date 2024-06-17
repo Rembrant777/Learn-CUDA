ca409bb5bac419df02c6d0eb0cbac3eab5972138
emma.xie <nanachi1027@163.com>
2024-06-17 23:01:25 +0800

    add layer and loss header fiiles

---
+++ Chapter10/01_ann/src/layer.h
@@ -0,0 +1,17 @@
+#ifndef _LAYER_H_
+#define _LAYER_H_
+
+#include <string>
+#include <cublas_v2.h>
+#include <cudnn.h>
+
+#include "blob.h"
+#include "loss.h"
+#include "helper.h"
+
+namespace cudl {
+
+
+ 
+} // namespace cudl 
+#endif // _LAYER_H_
---
+++ Chapter10/01_ann/src/loss.h
@@ -0,0 +1,27 @@
+#ifndef _LOSS_H_
+#define _LOSS_H_
+
+#include "blob.h"
+namespace cudl { 
+
+class CrossEntropyLoss {
+public:
+    CrossEntropyLoss(); 
+    ~CrossEntropyLoss(); 
+
+    float loss(Blob<float> *predict, Blob<float> *target); 
+    float accuracy(Blbg<float> *predict, Blog<float> *target);
+
+private:
+    // reduced loss 
+    // todo here we can support more loss functions 
+    float h_loss_ = 0.f; 
+    float *d_loss = nullptr; 
+    float *d_workspace_ = nullptr; 
+    void init_workspace(int batch_size); 
+
+}; // class CrossEntropyLoss    
+
+} // namespace cudl 
+
+#endif  // _LOSS_H_
