e761f2b6ef679fe19029418909a5241a768254a7
Rembrandt <nanachi1027@163.com>
2024-06-17 08:57:51 +0000

    add dataset

--- Chapter10/01_ann/download_mnist.sh
+++ Chapter10/01_ann/download_mnist.sh
@@ -5,9 +5,9 @@ url_base=http://yann.lecun.com/exdb/mnist
 mkdir -p dataset
 cd dataset
 
-curl -O ${url_base}/train-images-idx3-ubyte.gz
-curl -O ${url_base}/train-labels-idx1-ubyte.gz
-curl -O ${url_base}/t10k-images-idx3-ubyte.gz
-curl -O ${url_base}/t10k-labels-idx1-ubyte.gz
+wget https://raw.githubusercontent.com/fgnt/mnist/master/train-images-idx3-ubyte.gz
+wget https://raw.githubusercontent.com/fgnt/mnist/master/train-labels-idx1-ubyte.gz
+wget https://raw.githubusercontent.com/fgnt/mnist/master/t10k-images-idx3-ubyte.gz
+wget https://raw.githubusercontent.com/fgnt/mnist/master/t10k-labels-idx1-ubyte.gz 
 
 gunzip *.gz
