1afce47a346e2e9ad31a747373d26fefdc030d34
emma.xie <nanachi1027@163.com>
2024-06-19 23:59:45 +0800

    add cudnn unit test cases

---
+++ Chapter10/00_cudnn/cudnn_test_00/src/hello_cudnn.cu
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "hello_cudnn.h"
+
+int main() 
+{
+    std::cout << "Hello CUDNN" << std::endl; 
+    return 0; 
+}
---
+++ Chapter10/00_cudnn/cudnn_test_00/src/hello_cudnn.h
@@ -0,0 +1,6 @@
+#ifndef HELLO_CUDNN_H
+#define HELLO_CUDNN_H
+
+// 这里可以添加你的函数声明和其他头文件内容
+
+#endif // HELLO_CUDNN_H
