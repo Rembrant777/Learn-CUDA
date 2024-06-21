58e1b92486dca3374c51e6723f5802b2130074c6
emma.xie <nanachi1027@163.com>
2024-06-21 18:09:00 +0800

    add backward operation

--- Chapter10/00_cudnn/cudnn_test_04/test/test_cudnn_sigmoid.cpp
+++ Chapter10/00_cudnn/cudnn_test_04/test/test_cudnn_sigmoid.cpp
@@ -31,6 +31,7 @@ public:
     }
 
     void destroyActivationDescriptor() {
+        std::cout << "#destroyActivationDescriptor " << std::endl; 
         if (cudnnDestroyActivationDescriptor(activate_descroptor_) != CUDNN_STATUS_SUCCESS) {
             throw std::runtime_error("Failed to destroy activation descriptor"); 
         }
@@ -52,7 +53,31 @@ public:
         }
     }
 
+    // here we implement MSE as the loss function 
+    // MSE (Mean Sequared Error)
+    // MSE = { ((y(1) - y'(1))^2 + ... + (y(n) - y'(n))^2 } /n, n is the number of sample 
+    float mseLossFunc(const float *predicted, const float* expected, int N) {
+        float ret = 0.f; 
+        for (int i = 0; i < N; i++) {
+            float diff = predicted[i] - expected[i]; 
+            std::cout << "diff " << diff << std::endl; 
+            ret += diff * diff;  
+        }
+
+        ret /= N; 
+        return ret; 
+    }
+
+    // here we implement MES's gradient function 
+    // MSE' = 2 * { ((y(1) - y'(1)) + ... + (y(n) - y'(n)) } / n
+    // MSE' = {2 * ((y(1) - y'(1)) + ... + 2 * ((y(n) - y'(n)) } / n 
     // retrieve class inner activation descriptor instance 
+    void mseLossGradient(const float *predicted, const float *expected, float *grad, int N) {
+        for (int i = 0; i < N; i++) {
+            grad[i] = 2 * (predicted[i] - expected[i]) / N; 
+        }
+    }
+
     cudnnActivationDescriptor_t getActivationDescriptor() {
         return activate_descroptor_; 
     }
@@ -60,7 +85,6 @@ public:
     cudnnHandle_t getCudnnHandle() {
         return cudnn_handle_; 
     }
-
 
 private:
     cudnnHandle_t               cudnn_handle_; 
@@ -72,31 +96,31 @@ private:
 }; 
 
 // enumeration: CUDNN_ACTIVATION_SIGMOID
-TEST(TestCudnnActivationDescriptor, CreateSigmoidActivationDescriptor) 
-{
-    cudnnActivationMode_t mode      = CUDNN_ACTIVATION_SIGMOID; 
-    cudnnNanPropagation_t nan_prop  = CUDNN_PROPAGATE_NAN; 
-    double coef                     = 0.0; 
-    EXPECT_EQ(mode, CUDNN_ACTIVATION_SIGMOID);
-    EXPECT_EQ(nan_prop, CUDNN_PROPAGATE_NAN);
+// TEST(TestCudnnActivationDescriptor, CreateSigmoidActivationDescriptor) 
+// {
+//     cudnnActivationMode_t mode      = CUDNN_ACTIVATION_SIGMOID; 
+//     cudnnNanPropagation_t nan_prop  = CUDNN_PROPAGATE_NAN; 
+//     double coef                     = 0.0; 
+//     EXPECT_EQ(mode, CUDNN_ACTIVATION_SIGMOID);
+//     EXPECT_EQ(nan_prop, CUDNN_PROPAGATE_NAN);
 
-    TestCudnnActivationDescriptor* instance = new TestCudnnActivationDescriptor(mode, nan_prop, coef);
-    EXPECT_NE(instance, nullptr); 
+//     TestCudnnActivationDescriptor* instance = new TestCudnnActivationDescriptor(mode, nan_prop, coef);
+//     EXPECT_NE(instance, nullptr); 
 
-    EXPECT_NO_THROW((*instance).createActivationDescriptor()); 
+//     EXPECT_NO_THROW((*instance).createActivationDescriptor()); 
 
-    cudnnActivationMode_t ret_mode; 
-    cudnnNanPropagation_t ret_nan_prop; 
-    double ret_coef; 
+//     cudnnActivationMode_t ret_mode; 
+//     cudnnNanPropagation_t ret_nan_prop; 
+//     double ret_coef; 
 
-    EXPECT_NO_THROW((*instance).getActivationDescriptor(ret_mode, ret_nan_prop, ret_coef)); 
+//     EXPECT_NO_THROW((*instance).getActivationDescriptor(ret_mode, ret_nan_prop, ret_coef)); 
 
-    EXPECT_EQ(ret_mode, mode); 
-    EXPECT_EQ(ret_nan_prop, nan_prop); 
-    EXPECT_EQ(ret_coef, coef); 
+//     EXPECT_EQ(ret_mode, mode); 
+//     EXPECT_EQ(ret_nan_prop, nan_prop); 
+//     EXPECT_EQ(ret_coef, coef); 
 
-    EXPECT_NO_THROW((*instance).destroyActivationDescriptor()); 
-}
+//     // EXPECT_NO_THROW((*instance).destroyActivationDescriptor()); 
+// }
 
 TEST(TestCudnnActivationDescriptor, ApplySigmoidActivationForward)
 {
@@ -124,12 +148,6 @@ TEST(TestCudnnActivationDescriptor, ApplySigmoidActivationForward)
                             };
 
     float output[size]; 
-    float grad_input[size]; 
-    float grad_output[size] = {
-                                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-                            };
 
     // create & cudnn tensor descriptor which used to define the meta data configuration info 
     cudnnTensorDescriptor_t tensor_desc; 
@@ -165,12 +183,108 @@ TEST(TestCudnnActivationDescriptor, ApplySigmoidActivationForward)
         std::cout << output[i] << " " << std::endl; 
     }                
     std::cout << std::endl; 
-
-    EXPECT_NO_THROW((*instance).destroyActivationDescriptor()); 
+    delete instance; 
 }
 
 
 TEST(TestCudnnActivationDescriptor, ApplySigmoidActivationBackforward)
 {
-    EXPECT_EQ(1, 1);
+    cudnnActivationMode_t mode      = CUDNN_ACTIVATION_SIGMOID; 
+    cudnnNanPropagation_t nan_prop  = CUDNN_PROPAGATE_NAN; 
+    double coef                     = 0.0; 
+    // create test instance 
+    TestCudnnActivationDescriptor* instance = new TestCudnnActivationDescriptor(mode, nan_prop, coef);
+    
+    // invoke test instance to init activation descriptor 
+    EXPECT_NO_THROW((*instance).createActivationDescriptor()); 
+    cudnnActivationDescriptor_t act_desc = (*instance).getActivationDescriptor(); 
+
+
+    const int batch_size = 2; 
+    const int channels   = 3; 
+    const int height     = 4; 
+    const int width      = 4; 
+    const int size       = batch_size * channels * height * width ; 
+
+    float input[size] = {
+                            1, -1, 2, -2, 0.5, -0.5, 1.5, -1.5, 0.1, -0.1, 2.1, -2.1, 0.2, -0.2, 2.2, -2.2,
+                            1, -1, 2, -2, 0.5, -0.5, 1.5, -1.5, 0.1, -0.1, 2.1, -2.1, 0.2, -0.2, 2.2, -2.2,
+                            1, -1, 2, -2, 0.5, -0.5, 1.5, -1.5, 0.1, -0.1, 2.1, -2.1, 0.2, -0.2, 2.2, -2.2
+                        };
+
+    float expected[size] = {
+                            0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+                            0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+                            0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5
+                        };  
+    float output[size]; 
+    float grad_input[size]; 
+    float grad_output[size];                                               
+
+    // create & cudnn tensor descriptor which used to define the meta data configuration info 
+    cudnnTensorDescriptor_t tensor_desc; 
+    cudnnCreateTensorDescriptor(&tensor_desc); 
+    // here the CUDNN_TENSOR_NCHW tells the cudnn descriptor initializer following parameters order is:
+    //  N(batch size) C(channel num) H(height) W(width)
+    cudnnSetTensor4dDescriptor(tensor_desc, CUDNN_TENSOR_NCHW, 
+                                CUDNN_DATA_FLOAT, batch_size, channels, height, width); 
+
+    
+    float alpha = 1.0f; 
+    float beta  = 0.0f; 
+    cudnnHandle_t cudnn = (*instance).getCudnnHandle(); 
+
+    EXPECT_NE(&cudnn, nullptr); 
+
+    // Invoke forward pass for activation: y = α × op(x) + β × y
+    // activation_desc provides the 'op' function's implementation (e.g., sigmoid, ReLU, etc.)
+    // tensor_desc provides the meta info like the number of rows and columns of the input and output matrices
+    // input provides the 'x' matrix
+    // alpha provides the value of 'α'
+    // beta provides the value of 'β'
+    // output provides the 'y' matrix
+    cudnnActivationForward(
+                cudnn, act_desc,
+                &alpha, tensor_desc, input, 
+                &beta, tensor_desc, output
+                ); 
+
+    // here print and verify the output of the forward pass 
+    std::cout << "Forward pass output:" << std::endl; 
+    for (int i = 0; i < size; i++) {
+        std::cout << output[i] << " " ; 
+    }                
+    std::cout << std::endl; 
+
+
+    // here we invoke loss function MES to calculate the loss value 
+    float loss = (*instance).mseLossFunc(output, expected, size); 
+    std::cout << "Loss Value " << loss << std::endl; 
+
+    // here we invoke gradient of loss function to calculate the gradient value array 
+    (*instance).mseLossGradient(output, expected, grad_output, size);
+    std::cout << "Loss Gradient output:" << std::endl;  
+    for (int i = 0; i < size; i++) {
+        std::cout << grad_output[i] << " "; 
+    }
+    std::cout << std::endl; 
+
+    cudnnDestroyTensorDescriptor(tensor_desc);
+
+    // since we already have grad_output and input 
+    // let's invoke the backforward operation 
+    cudnnActivationBackward(
+        cudnn, act_desc,
+        &alpha, tensor_desc, output, tensor_desc, grad_output,
+        tensor_desc, input, &beta, tensor_desc, grad_input
+    );
+
+    // here we print the grad_input value 
+    std::cout << "Backward pass output (gradient w.r.t input): " << std::endl; 
+    for (int i = 0; i < size; i++) {
+        std::cout << grad_input[i] << " "; 
+    }
+    std::cout << std::endl; 
+
+    delete instance;
 }
