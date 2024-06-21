c1859043631248d988e85b8062b657152d371792
emma.xie <nanachi1027@163.com>
2024-06-21 16:07:33 +0800

    add sigmoid as activation function to execute forward operation

--- Chapter10/00_cudnn/cudnn_test_04/test/test_cudnn_sigmoid.cpp
+++ Chapter10/00_cudnn/cudnn_test_04/test/test_cudnn_sigmoid.cpp
@@ -41,10 +41,26 @@ public:
     // method to retrieve Activation Descriptor Options from Cudnn Context Env Configure Options. 
     void getActivationDescriptor(cudnnActivationMode_t &ret_mode, 
                             cudnnNanPropagation_t &ret_nan_prop, double &ret_coef) const {
+
+        // if  not created return without set values                                 
+        if (!is_descriptor_created_) {
+            return; 
+        }                                            
+
         if (cudnnGetActivationDescriptor(activate_descroptor_, &ret_mode, &ret_nan_prop, &ret_coef) != CUDNN_STATUS_SUCCESS) {
             throw std::runtime_error("Failed to set activation descriptor"); 
         }
     }
+
+    // retrieve class inner activation descriptor instance 
+    cudnnActivationDescriptor_t getActivationDescriptor() {
+        return activate_descroptor_; 
+    }
+
+    cudnnHandle_t getCudnnHandle() {
+        return cudnn_handle_; 
+    }
+
 
 private:
     cudnnHandle_t               cudnn_handle_; 
@@ -84,7 +100,73 @@ TEST(TestCudnnActivationDescriptor, CreateSigmoidActivationDescriptor)
 
 TEST(TestCudnnActivationDescriptor, ApplySigmoidActivationForward)
 {
-    EXPECT_EQ(1, 1);
+    cudnnActivationMode_t mode      = CUDNN_ACTIVATION_SIGMOID; 
+    cudnnNanPropagation_t nan_prop  = CUDNN_PROPAGATE_NAN; 
+    double coef                     = 0.0; 
+    // create test instance 
+    TestCudnnActivationDescriptor* instance = new TestCudnnActivationDescriptor(mode, nan_prop, coef);
+    
+    // invoke test instance to init activation descriptor 
+    EXPECT_NO_THROW((*instance).createActivationDescriptor()); 
+    cudnnActivationDescriptor_t act_desc = (*instance).getActivationDescriptor(); 
+
+
+    const int batch_size = 2; 
+    const int channels   = 3; 
+    const int height     = 4; 
+    const int width      = 4; 
+    const int size       = batch_size * channels * height * width ; 
+
+    float input[size]   =  {
+                                1, -1, 2, -2, 0.5, -0.5, 1.5, -1.5, 0.1, -0.1, 2.1, -2.1, 0.2, -0.2, 2.2, -2.2,
+                                1, -1, 2, -2, 0.5, -0.5, 1.5, -1.5, 0.1, -0.1, 2.1, -2.1, 0.2, -0.2, 2.2, -2.2,
+                                1, -1, 2, -2, 0.5, -0.5, 1.5, -1.5, 0.1, -0.1, 2.1, -2.1, 0.2, -0.2, 2.2, -2.2
+                            };
+
+    float output[size]; 
+    float grad_input[size]; 
+    float grad_output[size] = {
+                                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+                            };
+
+    // create & cudnn tensor descriptor which used to define the meta data configuration info 
+    cudnnTensorDescriptor_t tensor_desc; 
+    cudnnCreateTensorDescriptor(&tensor_desc); 
+    // here the CUDNN_TENSOR_NCHW tells the cudnn descriptor initializer following parameters order is:
+    //  N(batch size) C(channel num) H(height) W(width)
+    cudnnSetTensor4dDescriptor(tensor_desc, CUDNN_TENSOR_NCHW, 
+                                CUDNN_DATA_FLOAT, batch_size, channels, height, width); 
+
+    
+    float alpha = 1.0f; 
+    float beta  = 0.0f; 
+    cudnnHandle_t cudnn = (*instance).getCudnnHandle(); 
+
+    EXPECT_NE(&cudnn, nullptr); 
+
+    // Invoke forward pass for activation: y = α × op(x) + β × y
+    // activation_desc provides the 'op' function's implementation (e.g., sigmoid, ReLU, etc.)
+    // tensor_desc provides the meta info like the number of rows and columns of the input and output matrices
+    // input provides the 'x' matrix
+    // alpha provides the value of 'α'
+    // beta provides the value of 'β'
+    // output provides the 'y' matrix
+    cudnnActivationForward(
+                cudnn, act_desc,
+                &alpha, tensor_desc, input, 
+                &beta, tensor_desc, output
+                ); 
+
+    // here print and verify the output of the forward pass 
+    std::cout << "Forward pass output:" << std::endl; 
+    for (int i = 0; i < size; i++) {
+        std::cout << output[i] << " " << std::endl; 
+    }                
+    std::cout << std::endl; 
+
+    EXPECT_NO_THROW((*instance).destroyActivationDescriptor()); 
 }
 
 
