6ca1ccf386ade4098c5afe2089e374d956aaf772
emma.xie <nanachi1027@163.com>
2024-06-25 10:48:11 +0800

    update script

--- Chapter10/01_ann/run_test_layer.sh
+++ Chapter10/01_ann/run_test_layer.sh
@@ -1,2 +1,6 @@
+#!/bin/sh 
+
+# export cuda library for test_layer 
 export LD_LIBRARY_PATH=/usr/local/cuda-9.2/lib64
+
 ./test_layer > test_layer.log
--- Chapter10/01_ann/test/test_layer.cpp
+++ Chapter10/01_ann/test/test_layer.cpp
@@ -52,12 +52,44 @@ TEST(Testlayer, DenseInitWeightBias) {
     d_layer->fwd_initialize(input); 
     
 
+    // init biases cannot be null 
     Blob<float>* p_biases = d_layer->biases_; 
     EXPECT_NE(p_biases, nullptr); 
+
+    // init weights cannot be null
     Blob<float>* p_weights = d_layer->weights_; 
     EXPECT_NE(p_weights, nullptr); 
 
-    d_layer->init_weight_bias(); 
+    // init input_ cannot be null 
+    // input's tensor (input data dimension meta info object) neither cannot be null 
+    EXPECT_EQ(input, d_layer->input_);
+    EXPECT_NE(nullptr, d_layer->input_->tensor()); 
+
+    // init output_ cannot be null 
+    EXPECT_NE(nullptr, d_layer->output_);
+    // output's tensor descriptor neither cannot be null
+    EXPECT_NE(nullptr, d_layer->output_->tensor()); 
+
+    // init forward will create & init d_one_vec 
+    EXPECT_NE(nullptr, d_layer->d_one_vec); 
+
+    // weight & bias init value are initialized in fwd_initialize 
+    // d_layer->init_weight_bias(); 
+    p_biases->print("bias-value", 1, 4); 
+    p_weights->print("weight-value", 1, 4); 
+
+
+    // here we check class variable load_pretrain_ 
+    // if load_pretrain_ = true it will load parameters(weight & bias) from file 
+    // otherwise dense layer will invoke init_weights_biases function to generate parameters and hold them in memory 
+    EXPECT_EQ(false, d_layer->load_pretrain_); 
+
+    // since every thing is ok, we mock the network layer's write its parameters to disk
+    // then let next layer to load parameter from the disk 
+    //int ret = d_layer->save_parameter(); 
+    // if parameters are write success, ret value should be 
+    // EXPECT_EQ(0, ret); 
+
 
     delete d_layer; 
 }
